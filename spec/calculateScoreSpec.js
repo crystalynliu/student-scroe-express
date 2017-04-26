da5569e8c1334e3a2a6ea0dfbe17be3f407c8e80
Xinqian liu <crystalynliu@163.com>
2017-04-26 16:34:27 +0800

    add test of calculate total average score about student total score and pass the test

--- spec/calculateScoreSpec.js
+++ spec/calculateScoreSpec.js
@@ -53,4 +53,52 @@ describe('test calculate Scores of students', function () {
     })
   })
 
+  describe('calculate all student total score and average score', function () {
+    it('check result is right when input the all students total scores', function () {
+      const studentsScore = [333, 250];
+      const expectResult = 291.5;
+      expect(calculateScore.calculateTotalAverageScore(studentsScore)).toEqual(expectResult);
+
+    })
+
+    it('check result is zero when input is an empty array', function () {
+      const studentsScore = [];
+      const expectResult = 0;
+      expect(calculateScore.calculateTotalAverageScore(studentsScore)).toEqual(expectResult);
+    })
+
+    it('check result is right when input is a number string array', function () {
+      const studentsScore = ['123', '456'];
+      const expectResult = 289.5;
+      expect(calculateScore.calculateTotalAverageScore(studentsScore)).toEqual(expectResult);
+    })
+
+    it('throw error when input is a not number string array', function () {
+      const studentsScore = ['abc', '++'];
+      expect(function(){
+        calculateScore.calculateTotalAverageScore(studentsScore)
+      }).toThrow('input is not a validate array');
+    })
+
+    it('throw error when inputs have an undefined value', function () {
+      const studentsScore = [undefined];
+      expect(function(){
+        calculateScore.calculateTotalAverageScore(studentsScore)
+      }).toThrow('input is not a validate array');
+    })
+
+    it('throw error when inputs have an null value', function () {
+      const studentsScore = [null];
+      expect(function(){
+        calculateScore.calculateTotalAverageScore(studentsScore)
+      }).toThrow('input is not a validate array');
+    })
+
+    it('throw error when input is not an array', function () {
+      const studentsScore = '123';
+      expect(function(){
+        calculateScore.calculateTotalAverageScore(studentsScore)
+      }).toThrow('input is not a validate array');
+    })
+  })
 })
--- src/calculateScore.js
+++ src/calculateScore.js
@@ -10,8 +10,24 @@ const calculateStudentScore = function (subjectScore) {
   return {averageScore: parseFloat(totalScore/count || 0), totalScore: totalScore}
 }
 
+const calculateTotalAverageScore = function (studentsTotal) {
+  if(!Array.isArray(studentsTotal)){
+    throw new TypeError('input is not a validate array');
+  }
+  let count = studentsTotal.length;
+  let allStudentTotalScore = studentsTotal.reduce((totalScore, studentScore) => {
+    if(isNaN(studentScore) || studentScore === null){
+      throw new TypeError('input is not a validate array');
+    }
+    return totalScore + parseFloat(studentScore);
+  }, 0);
+  return parseFloat(allStudentTotalScore/count || 0);
+}
+
+
 const calculateScore = {
-  calculateStudentScore: calculateStudentScore
+  calculateStudentScore: calculateStudentScore,
+  calculateTotalAverageScore: calculateTotalAverageScore
 }
 
 module.exports = calculateScore;
